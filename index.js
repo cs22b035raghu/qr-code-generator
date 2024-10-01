861753336b38f7cbb3900b2e1ba82a8c6fa0dda3
cs22b035raghu <161196863+cs22b035raghu@users.noreply.github.com>
2024-10-02 02:29:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+ const questions=[
+    {
+        type:'input',
+        name:'web-address',
+        message:'type your address',
+
+
+    },
+    {
+       type:'input',
+       name:'web-2',
+       message:'enter another address'
+
+    },
+
+];
+
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+
+    const qrCodeData = qr.image(answers['web-address'], { type: 'png' });
+    qrCodeData.pipe(fs.createWriteStream('qrcode.png'));
+
+    const qrCodeData1 = qr.image(answers['web-2'], { type: 'png' });
+    qrCodeData1.pipe(fs.createWriteStream('qrcode1.png'));
+   
+    const data=answers['web-address']+"\n"+answers['web-2'];
+     
+     fs.writeFile('example.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+  });
+
